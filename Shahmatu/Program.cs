5b669b89c71bf0cc60f30fe40e56d32009ea3c22
Venus <bisnesbrend@gmail.com>
2022-08-18 20:12:58 +0300

    Создал поле

--- Shahmatu/Program.cs
+++ Shahmatu/Program.cs
@@ -8,8 +8,43 @@ namespace Shahmatu
 {
     internal class Program
     {
+        static int[,] map =
+        {
+            { 0, 1, 0, 0, 0, 0, 0 , 0},
+            { 0, 0, 0, 0, 0, 0, 0 , 1},
+            { 0, 0, 0, 0, 0, 0, 0 , 1},
+            { 0, 0, 0, 0, 0, 0, 0 , 1},
+            { 0, 0, 0, 0, 0, 0, 0 , 1},
+            { 0, 0, 0, 0, 0, 0, 0 , 1},
+            { 0, 0, 0, 0, 0, 0, 0 , 1},
+            { 0, 0, 0, 0, 0, 0, 0 , 1}
+
+        };
+
+        static void Show()
+        {
+            for (int i = 0; i < map.GetLength(0); i++)
+            {
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i, j] == 1)
+                    {
+                        Console.Write(map[i, j]);
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+
+
         static void Main(string[] args)
         {
+            Show();
+            Console.ReadKey();
         }
     }
 }
